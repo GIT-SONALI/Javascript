cc0b542e684217579db0bebedbf9b1d7a96da23c
GIT-SONALI <mohantysonali479@gmail.com>
2025-01-21 15:20:27 +0000

    variables declaration done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "sonali@gmail.com"
+var acountPassword = "12345"
+accountCity = "Ichalkaranji"
+let accountState;
+
+//accountId=2 //not allowed
+
+
+
+accountEmail ="son@gmail.com"
+accountPassword = "2331344"
+accountCity = "Pune"
+
+console.log("accountId");
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
