454a6d3556d1534f4a23966ef1220080f47112fb
GIT-SONALI <mohantysonali479@gmail.com>
2025-01-22 07:18:52 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 4) //we are using node js not brower
+
+console.log( 3
+    +
+    3); //code readability should be high
+
+console.log("Sonali")
+
+let name="sonali"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number => 2 to power 53
+// bigint => for larger values
+// string =>""
+// boolean => true/false
+// null => (Standalone value) defined the variable is null not empty
+// undefined => defined the variable is not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);  //number datatype
+
+console.log(typeof null);  //object datatype // *important for interview question
+
+console.log(typeof undefined); // undefined datatype
+
